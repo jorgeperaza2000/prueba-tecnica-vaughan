f65aceab79e66468d5cca0b2a0dfedbc91af4f4d
jorgeperaza <jorgeperaza2000@gmail.com>
2024-11-24 19:14:33 +0100

    ft/create Bearer Authorization Middleware, api route files

--- app/Http/Middleware/v1/BearerAuthorization.php
+++ app/Http/Middleware/v1/BearerAuthorization.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware\v1;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 
 class BearerAuthorization
@@ -15,6 +16,67 @@ class BearerAuthorization
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if ($this->authorizationHeaderIsNotPresent($request)) {
+
+            throw ValidationException::withMessages([
+                'Authorization' => ['The Authorization header is missing.'],
+            ]);
+        }
+
+        if ($this->authorizationHeaderNotContainBearer($request)) {
+
+            throw ValidationException::withMessages([
+                'Authorization' => ['The Authorization header is invalid.'],
+            ]);
+        }
+
+        if ($this->isValidParentheses($request->header('Authorization'))) {
+
+            throw ValidationException::withMessages([
+                'Authorization' => ['The Authorization token is invalid.'],
+            ]);
+        }
+
         return $next($request);
+    }
+
+    private function authorizationHeaderIsNotPresent(Request $request): bool
+    {
+
+        return ! $request->hasHeader('Authorization');
+    }
+
+    private function authorizationHeaderNotContainBearer(Request $request): bool
+    {
+
+        return ! str_starts_with($request->header('Authorization'), 'Bearer ');
+    }
+
+    private function isValidParentheses(string $authorization): bool
+    {
+        $token = trim(str_replace('Bearer', '', $authorization));
+
+        $allowedCharacters = [
+            ')' => '(',
+            '}' => '{',
+            ']' => '[',
+        ];
+
+        $openingChars = [];
+        foreach (str_split($token) as $char) {
+
+            if (in_array($char, $allowedCharacters)) {
+
+                $openingChars[] = $char;
+            } elseif (isset($allowedCharacters[$char])) {
+
+                if (array_pop($openingChars) !== $allowedCharacters[$char]) {
+
+                    return true;
+                }
+            }
+        }
+
+        return ! empty($openingChars);
     }
 }
--- routes/v1/api.php
+++ routes/v1/api.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\v1\BearerAuthorization;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/short-urls', function () {
+Route::middleware([BearerAuthorization::class])->group(function () {
+
     return response('TODO: implement short-urls', 200);
-})->middleware(BearerAuthorization::class);
+});
